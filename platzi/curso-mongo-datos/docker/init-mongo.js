9fe5320cc4141116ab996745ced07041338ba453
edzamo13@gmail.com <ezamora@mag.gob.ec>
2023-02-02 17:47:39 -0500

    change .git

--- platzi/curso-mongo-datos/docker/init-mongo.js
+++
@@ -1,8 +0,0 @@
-db.createUser({
-  user: 'nestuser',
-  pwd: 'nestuser',
-  roles: [{
-    role: 'readWrite',
-    db: 'nestdb',
-  }],
-})
--- platzi/curso-mongo-datos/src/config/index.ts
+++
@@ -1,9 +0,0 @@
-const config = {
-  DATABASE_URL: process.env.DATABASE_URL_DEV,
-}
-
-if (process.env.NODE_ENV === 'test') {
-  config.DATABASE_URL = process.env.DATABASE_URL_TEST
-}
-
-export default config;
